96536bebeba7f150982f74af3e72c46d904deb87
Andrés Arnedo Escalante <79600570+ArnedoAn@users.noreply.github.com>
2023-03-15 23:18:56 +0000

    .

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
 from flask_pyjwt import AuthManager, require_token
 from flask_swagger_ui import get_swaggerui_blueprint
+
 app = Flask(__name__)
 auth_manager = AuthManager(app)
-
 
 from entities.nodo import *
 from entities.jwt_auth import *
@@ -11,7 +11,7 @@ from entities.user import *
 from controllers.login_controller import *
 
 # Configuración de Swagger UI
-SWAGGER_URL = '/'  # URL para acceder a la documentación Swagger
+SWAGGER_URL = '/apidocs'  # URL para acceder a la documentación Swagger
 API_URL = '/static/swagger.json'  # URL de la especificación Swagger/OpenAPI
 
 # Creación del blueprint de Swagger UI
@@ -25,6 +25,10 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 
 # Registro del blueprint en la aplicación Flask
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 @app.route('/login', methods=['POST'])
 def login_post():
@@ -40,3 +44,6 @@ def test():
 def register_post():
     print(request.get_json())
     return register(request.get_json())
+
+if __name__ == '__main__':
+    app.run()
